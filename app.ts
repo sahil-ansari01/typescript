f65bd38423c51cc2bb034e5b3a5198bbe38cd293
sahil-ansari01 <opensourcecarrier@gmail.com>
2024-06-23 07:36:03 +0530

    Integrating Typescript with Node and Express!

---
+++ app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { Router } from 'express';
+
+import todosRoutes from './routes/todos';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(todosRoutes);
+
+app.listen({ port: 3000 });
